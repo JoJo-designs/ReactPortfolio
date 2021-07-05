3ff7442e19e67a5a02e1b8262a22172200fbb01c
jojo <jodiladouceur.design@gmail.com>
2021-07-05 10:35:02 -0400

    adding some simple react files

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './nav';
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Hello this is my react portfoilo</h1>
+            <Nav />
+        </div>
+        
+    );
+}
---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <div>
+            <h3>About Me</h3>
+            <h3>Portfolio</h3>
+            <h3>Contact</h3>
+            <h3>Resume</h3>
+        </div>
+    );
+}
