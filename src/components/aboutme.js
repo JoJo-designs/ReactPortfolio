542481deab38248ef8e8568139a9cd2530304833
jojo <jodiladouceur.design@gmail.com>
2021-07-08 10:54:57 -0400

    adding more components

---
+++ src/components/aboutme.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AboutMe() {
+    return (
+        <div>
+            <h2>About me</h2>
+            <p>this is some text about me and why people should hire me I guess</p>
+        </div>
+    )
+}
