6eb77642a90659bfa0171598209a70eb9461a163
jojo <jodiladouceur.design@gmail.com>
2021-07-12 16:36:14 -0400

    adding hovers

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -11,21 +11,13 @@ export default function AboutMe() {
             <aside className="sixty">
             <h2>About me</h2>
             <h3>Full Stack web development & Graphic Designer</h3>
-            <p>this is some text about me and why people should hire me I guess. Lorem Ipsum is simply dummy
-                 text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-                 dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-                 it to make a type specimen book. It has survived not only five centuries, but also the leap 
-                 into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
-                 with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
-                 desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <p>As a graphic designer and full-stack developer, I have a unique set of skills that 
+                allow me to work on a project from beginning to end.  I am interested in opportunities
+                 where I can use a little bit of all these skills. I want to help build a creative 
+                 world with other like-minded people. 
             </p>
-            <p>this is some text about me and why people should hire me I guess. Lorem Ipsum is simply dummy
-                 text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-                 dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-                 it to make a type specimen book. It has survived not only five centuries, but also the leap 
-                 into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
-                 with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
-                 desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <p>Outside of design, I enjoy spending my doing creative things. I like to draw and play 
+                guitar. I also enjoy reading manga and playing video games. 
             </p>
             </aside>
         </div>
