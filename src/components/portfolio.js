b403c153b6fcc961a6a47ffacbecdaf4189a7577
JoJo-designs <jodiladouceur.design@gmail.com>
2021-09-24 09:48:10 -0400

    edits to portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Featured from './featured';
+import { SocialIcon } from 'react-social-icons';
 import '../styles/portfolio.css'
 
 //images are 400 x 458 ish check later.
@@ -9,48 +10,56 @@ const portfoilo = [
         image: "./images/img8.jpg",
         alt: "Weather app",
         a: "https://jojo-designs.github.io/weatherApp/",
+        gitA: "https://github.com/JoJo-designs/weatherApp",
     },
     {
         imageId: 2,
         image: "./images/img7.jpg",
         alt: "pet finder app",
         a: "https://jojo-designs.github.io/The-Ultimate-Personality-Pet-Finder/",
+        gitA: "https://github.com/JoJo-designs/The-Ultimate-Personality-Pet-Finder",
     },
     {
         imageId: 3,
         image: "./images/img9.jpg",
         alt: "app that can generate a strong password",
         a: "https://jojo-designs.github.io/securePasswordGenerator/",
+        gitA: "https://github.com/JoJo-designs/securePasswordGenerator",
     },
     {
         imageId: 4,
         image: "./images/img10.jpg",
         alt: "app",
         a: "https://jojo-designs.github.io/calanderApp/",
+        gitA: "https://github.com/JoJo-designs/calanderApp",
     },
     {
         imageId: 5,
         image: "./images/img11.jpg",
         alt: "app that will allow people to keep track of the work day by hour.",
         a: "https://jodinotetaker.herokuapp.com/",
+        gitA: "https://github.com/JoJo-designs/note-Tracker",
     },
     {
         imageId: 6,
         image: "./images/img12.jpg",
         alt: "ottawa sport field booking app",
         a: "https://lit-spire-78703.herokuapp.com/",
+        gitA: "https://github.com/FieldBookingSystem/FieldBookingSystem",
     },
     {
         imageId: 7,
         image: "./images/img15.jpg",
         alt: "fitness tracker app",
         a: "https://cool-fitness.herokuapp.com/",
+        gitA: "https://github.com/JoJo-designs/fitnessTracker",
     },
     {
         imageId: 8,
         image: "./images/img17.jpg",
         alt: "A Tech Dashboard app",
         a: "https://super-tech-dashboard.herokuapp.com/",
+        gitA: "https://github.com/JoJo-designs/tech-dashboard",
     },
 ]
 
@@ -60,7 +69,11 @@ export default function Portfolio() {
             <div className="featured"><Featured /></div>
             <div className="flexing">
                 {portfoilo.map((project) => (
-               <a className="linkflex" key={project.imageId} href={project.a}><img src={project.image} alt={project.alt} className="imgGrid"></img></a> 
+              <div className="linkflex hovering"> <a key={project.imageId} href={project.a}>
+                   <img src={project.image} alt={project.alt} className="imgGrid"></img>
+                   </a> 
+                   <div className="overLap"><SocialIcon url={project.gitA} bgColor="none" fgColor="rgb(209, 88, 88)"/></div>
+                </div>
             ))}
             </div>
         </div>
