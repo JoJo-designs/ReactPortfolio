4aa5faa2b614daf2e4bbbbe5d203d7386380b03d
JoJo-designs <jodiladouceur.design@gmail.com>
2021-09-24 10:18:28 -0400

    edit to featuered

--- src/components/featured.js
+++ src/components/featured.js
@@ -5,7 +5,7 @@ export default function Featured() {
     return (
         <div>
            <a href=" https://jojo-designs.github.io/SuperCodeQuiz/">
-            <img className="featflex" src="images/img3.png" alt="a website landing page which starts a quiz about codeing"></img>
+            <img className="featflex" src="images/img3.png" alt="a website landing page which starts a quiz about coding"></img>
             </a>
             <div className="overLap"><SocialIcon url="https://github.com/JoJo-designs/SuperCodeQuiz" bgColor="none" fgColor="rgb(209, 88, 88)"/></div>
         </div>
