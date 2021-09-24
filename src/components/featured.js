05f207e86898c445ec57f165a8b8da1cdaf3e840
JoJo-designs <jodiladouceur.design@gmail.com>
2021-09-24 10:07:30 -0400

    adding additional link to featured

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 export default function Featured() {
     return (
         <div>
-           <a href=" https://jojo-designs.github.io/SuperCodeQuiz/"><img className="featflex" src="images/img3.png" alt="a website landing page which starts a quiz about codeing"></img></a>
+           <a href=" https://jojo-designs.github.io/SuperCodeQuiz/">
+            <img className="featflex" src="images/img3.png" alt="a website landing page which starts a quiz about codeing"></img>
+            </a>
+            <div className="overLap"><SocialIcon url="https://github.com/JoJo-designs/SuperCodeQuiz" bgColor="none" fgColor="rgb(209, 88, 88)"/></div>
         </div>
     )
 }
