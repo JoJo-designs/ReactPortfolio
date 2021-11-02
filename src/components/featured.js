418d2ca379c6920a8cef41b767e6ae791df265f7
JoJo-designs <jodiladouceur.design@gmail.com>
2021-11-02 10:23:23 -0400

    fixing images

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import feat from "./image/img16.png"
 
 export default function Featured() {
     return (
         <div>
            <a href=" https://jojo-designs.github.io/SuperCodeQuiz/">
-            <img className="featflex" src="images/img3.png" alt="a website landing page which starts a quiz about coding"></img>
+            <img className="featflex" src={feat} alt="a website landing page which starts a quiz about coding"></img>
             </a>
             <div className="overLap"><SocialIcon url="https://github.com/JoJo-designs/SuperCodeQuiz" bgColor="none" fgColor="rgb(209, 88, 88)"/></div>
         </div>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,61 +2,70 @@ import React from 'react';
 import Featured from './featured';
 import { SocialIcon } from 'react-social-icons';
 import '../styles/portfolio.css'
+import weather from "./image/img8.jpg"
+import pets from "./image/img7.jpg"
+import password from "./image/img9.jpg"
+import work from "./image/img10.jpg"
+import note from "./image/img11.jpg"
+import field from "./image/img12.jpg"
+import fitness from "./image/img15.jpg"
+import tech from "./image/img17.jpg"
+
 
 //images are 400 x 458 ish check later.
 const portfoilo = [
     {
         imageId: 1,
-        image: "./images/img8.jpg",
+        image: weather,
         alt: "Weather app",
         a: "https://jojo-designs.github.io/weatherApp/",
         gitA: "https://github.com/JoJo-designs/weatherApp",
     },
     {
         imageId: 2,
-        image: "./images/img7.jpg",
+        image: pets,
         alt: "pet finder app",
         a: "https://jojo-designs.github.io/The-Ultimate-Personality-Pet-Finder/",
         gitA: "https://github.com/JoJo-designs/The-Ultimate-Personality-Pet-Finder",
     },
     {
         imageId: 3,
-        image: "./images/img9.jpg",
+        image: password,
         alt: "app that can generate a strong password",
         a: "https://jojo-designs.github.io/securePasswordGenerator/",
         gitA: "https://github.com/JoJo-designs/securePasswordGenerator",
     },
     {
         imageId: 4,
-        image: "./images/img10.jpg",
+        image: work,
         alt: "app",
         a: "https://jojo-designs.github.io/calanderApp/",
         gitA: "https://github.com/JoJo-designs/calanderApp",
     },
     {
         imageId: 5,
-        image: "./images/img11.jpg",
+        image: note,
         alt: "app that will allow people to keep track of the work day by hour.",
         a: "https://jodinotetaker.herokuapp.com/",
         gitA: "https://github.com/JoJo-designs/note-Tracker",
     },
     {
         imageId: 6,
-        image: "./images/img12.jpg",
+        image: field,
         alt: "ottawa sport field booking app",
         a: "https://lit-spire-78703.herokuapp.com/",
         gitA: "https://github.com/FieldBookingSystem/FieldBookingSystem",
     },
     {
         imageId: 7,
-        image: "./images/img15.jpg",
+        image: fitness,
         alt: "fitness tracker app",
         a: "https://cool-fitness.herokuapp.com/",
         gitA: "https://github.com/JoJo-designs/fitnessTracker",
     },
     {
         imageId: 8,
-        image: "./images/img17.jpg",
+        image: tech,
         alt: "A Tech Dashboard app",
         a: "https://super-tech-dashboard.herokuapp.com/",
         gitA: "https://github.com/JoJo-designs/tech-dashboard",
