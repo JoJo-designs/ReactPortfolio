bd2b0f9a27ef937bb8a70343f30e605f26158e9e
JoJo-designs <jodiladouceur.design@gmail.com>
2021-11-02 10:03:18 -0400

    testing to see if images work

--- src/components/aboutimage.js
+++ src/components/aboutimage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import me from "./image/me.png"
 
 export default function AboutImage() {
     return (
         <div>
-            <img className="profileImage" src="images/me.png" alt="this is me"></img>
+            <img className="profileImage" src={me} alt="this is me"></img>
         </div>
     )
 }
