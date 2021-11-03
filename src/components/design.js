397a22fad8eddf8123d9889d1ef294ffd520f893
JoJo-designs <jodiladouceur.design@gmail.com>
2021-11-03 11:44:52 -0400

    edit out some stuff from component

--- src/components/design.js
+++ src/components/design.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import colour from "./image/design1.jpg"
+// import colour from "./image/design1.jpg"
 
 
-const design = [
-    {
-        imageID: 1,
-        image: colour,
-        alt: "colour book cover",
-        href: "https://jojo-designs.github.io/portfoilo_helper/project1.html",
-    },
-]
+// const design = [
+//     {
+//         imageID: 1,
+//         image: colour,
+//         alt: "colour book cover",
+//         href: "https://jojo-designs.github.io/portfoilo_helper/project1.html",
+//     },
+// ]
 
 export default function Design() {
     return (
