2f0488bb8d2eadf6726bff9382bb8baf322ca15a
JoJo-designs <jodiladouceur.design@gmail.com>
2022-01-14 09:19:23 -0500

    attempt to make the new page work

--- src/components/design.js
+++ src/components/design.js
@@ -14,7 +14,7 @@ import React from 'react';
 export default function Design() {
     return (
         <div className="inner">
-            <h1>Graphic Deign Work Coming Soon</h1>   
+            <h1>Graphic Deign Work Coming Soon. If I can get this to work.</h1>   
         </div>
     )
 }
