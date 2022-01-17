01667bd61055c3f43c33e09ec2a0c89374d0247e
JoJo-designs <jodiladouceur.design@gmail.com>
2022-01-17 09:26:20 -0500

    add design prjects

--- src/components/design.js
+++ src/components/design.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import colour from "./image/design1.jpg"
+import '../styles/portfolio.css';
+import colour from "./image/design1.jpg";
+import decora from "./image/design2.jpg";
+import exhibition from "./image/design3.jpg"
+
 
 
 const design = [
     {
         imageID: 1,
         image: colour,
+        alt: "colour book cover",
+        href: "https://jojo-designs.github.io/portfoilo_helper/project1.html",
+    },
+    {
+        imageID: 2,
+        image: decora,
+        alt: "colour book cover",
+        href: "https://jojo-designs.github.io/portfoilo_helper/project1.html",
+    },
+    {
+        imageID: 3,
+        image: exhibition,
         alt: "colour book cover",
         href: "https://jojo-designs.github.io/portfoilo_helper/project1.html",
     },
@@ -18,7 +34,7 @@ export default function Design() {
                 {design.map((project) => (
                     <div className="linkflex">
                         <a key={project.imageID} href={project.href} target="_blank">
-                            <img src={project.image} alt={project.alt}></img>
+                            <img src={project.image} alt={project.alt} className="imgGrid"></img>
                         </a>
                     </div>
                 ))}
